ee0ac34b3861fa46e1630b8cbc09173284cbdb52
Julia Gabriela <juliagabrielagomes09@gmail.com>
2025-06-07 10:57:13 -0300

    Aula_10_07_06_25

---
+++ Aula_10_07_06_25/users.sql
@@ -0,0 +1,13 @@
+CREATE DATABASE userdb2;
+
+USE userdb2;
+
+CREATE TABLE users (
+	id INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL
+);
+
+select * from users;
+
+INSERT INTO users (nome, email) VALUES ('maria', 'maria@vaicomasoutras.vai');
